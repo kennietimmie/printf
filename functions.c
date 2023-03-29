870ccef8d991b8a611bef359e33316c65aa11eeb
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-29 19:51:14 +0100

    add write_sign

--- functions.c
+++ functions.c
@@ -9,3 +9,19 @@ void write_char(char c)
 {
 	write(1, &c, 1);
 }
+
+/**
+ * write_sign - write a negative sign to standard out
+ * @num: int
+ *
+ * Return: void
+ */
+void write_sign(int num)
+{
+        if (num < 0)
+        {
+                char c = '-';
+
+                write(1, &c, 1);
+        }
+}
